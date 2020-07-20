15d469573812465af1430abcb7fbf6bdb0136e0f
Vitalii Sosin <375693@mail.ru>
2020-07-20 23:09:28 +0300

    Initial Commit

---
+++ Traffic lights UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic lights UI
+//
+//  Created by Виталий Сосин on 20.07.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
